d711dae1b33c7859f4d442d67318b61e39326af1
marina <marina.gherman@icloud.com>
2021-11-08 11:18:02 +0100

    add axios

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import styles from './users.module.css'
+import  axios from "axios";
+import userPhoto from '../../assets/images/avatar.png'
 
-const Users = (props:any) => {
-    props.setUsers( [  {id:1, followed: false, photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyM1UJLHjiRFSx7VbTGBUTdsiUJJ9OLYyPEwKW7VfQsTRDLC6BHbsWqdccqFdewpOwBTU&usqp=CAU",
-        fullName: "Marina", status: "i am a boss", location: {city: 'Como',country: 'Italy'}},
-        {id:2, followed: false, photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyM1UJLHjiRFSx7VbTGBUTdsiUJJ9OLYyPEwKW7VfQsTRDLC6BHbsWqdccqFdewpOwBTU&usqp=CAU",
-            fullName: "Andrei", status: "i am a big boss", location: {city: 'Preganziol',country: 'Italy'}},
-        {id:3, followed: true,photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyM1UJLHjiRFSx7VbTGBUTdsiUJJ9OLYyPEwKW7VfQsTRDLC6BHbsWqdccqFdewpOwBTU&usqp=CAU",
-            fullName: "Dmitry", status: "Hello world", location: {city: 'Minsk',country: 'Belorus'}}])
+
+
+const Users = (props: any) => {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response: any) => {
+                props.setUsers(response.data.items)
+            });
+    }
     return (
         <>
             {
-                props.users.map((m:any)=> <div key={m.id}>
+                props.users.map((m: any) => <div key={m.id}>
                     <span>
                         <div className={styles.users}>
                             <img className={styles.usersPhoto}
-                                 src={m.photoURL}
+                                 src={m.photos.small != null ? m.photos.small : userPhoto }
                                  alt=""/>
                         </div>
                         <div>
                             {m.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(m.id)}}>UnFollow</button>
-                                : <button onClick={() => {props.follow(m.id)}}>Follow</button>
+                                    props.unfollow(m.id)
+                                }}>UnFollow</button>
+                                : <button onClick={() => {
+                                    props.follow(m.id)
+                                }}>Follow</button>
                             }
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{m.fullName}</div>
+                            <div>{m.name}</div>
                             <div>{m.status}</div>
                         </span>
                         <span>
-                            <div>{m.location.country}</div>
-                            <div>{m.location.city}</div>
+                            <div>{'m.location.country'}</div>
+                            <div>{'m.location.city'}</div>
                         </span>
                     </span>
                 </div>)
