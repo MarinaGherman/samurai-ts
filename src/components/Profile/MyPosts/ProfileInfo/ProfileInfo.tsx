cf0771ac71608c7d1883c75842110eb9d4278512
marina <marina.gherman@icloud.com>
2021-11-10 18:43:18 +0100

    lesson 59(refactor ProfileInfo)

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -6,10 +6,24 @@ import facebook from "../../../../assets/images/fb.png";
 import website from "../../../../assets/images/global.png";
 import  vk from "../../../../assets/images/vk.png";
 import twitter from "../../../../assets/images/twitter.png";
-import insta from "../../../../assets/images/instagram.png";
+import instagram from "../../../../assets/images/instagram.png";
 import youtube from "../../../../assets/images/youtube.png";
-import git from "../../../../assets/images/github.png";
-import mail from "../../../../assets/images/mail.png";
+import github from "../../../../assets/images/github.png";
+import mainLink from "../../../../assets/images/mail.png";
+
+
+const images: any = {
+    facebook: facebook,
+    website: website,
+    vk: vk,
+    twitter: twitter,
+    instagram: instagram,
+    youtube: youtube,
+    github: github,
+    mainLink: mainLink
+}
+
+const contacts = Object.keys(images)
 
 
 const ProfileInfo = (props:any) => {
@@ -35,14 +49,21 @@ const ProfileInfo = (props:any) => {
                 <h3>{props.profile.aboutMe}</h3>
             </div>
             <div className={s.contacts}>
-                {props.profile.contacts.facebook ? <a href={props.profile.contacts.facebook}> <img className={s.socialIcons} src={facebook} alt=""/></a> : ''}
-                {props.profile.contacts.website ? <a href={props.profile.contacts.website}> <img className={s.socialIcons} src={website} alt=""/></a> : ''}
-                {props.profile.contacts.vk ? <a href={props.profile.contacts.vk}> <img className={s.socialIcons} src={vk} alt=""/></a> : ''}
-                {props.profile.contacts.twitter ? <a href={props.profile.contacts.twitter}> <img className={s.socialIcons} src={twitter} alt=""/></a> : ''}
-                {props.profile.contacts.instagram ? <a href={props.profile.contacts.instagram}> <img className={s.socialIcons} src={insta} alt=""/></a> : ''}
-                {props.profile.contacts.youtube ? <a href={props.profile.contacts.youtube}> <img className={s.socialIcons} src={youtube} alt=""/></a> : ''}
-                {props.profile.contacts.github ? <a href={props.profile.contacts.github}> <img className={s.socialIcons} src={git} alt=""/></a> : ''}
-                {props.profile.contacts.mailLink ? <a href={props.profile.contacts.mailLink}> <img className={s.socialIcons} src={mail} alt=""/></a> : ''}
+                {contacts.map(contact => {
+                    if (Object.keys(props.profile.contacts).includes(contact) && props.profile.contacts[contact]) {
+                        return <a href={props.profile.contacts[contact]}> <img className={s.socialIcons} src={images[contact]} alt=""/></a>
+                    }
+
+                    return null;
+                })}
+                {/*{props.profile.contacts.facebook ? <a href={props.profile.contacts.facebook}> <img className={s.socialIcons} src={facebook} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.website ? <a href={props.profile.contacts.website}> <img className={s.socialIcons} src={website} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.vk ? <a href={props.profile.contacts.vk}> <img className={s.socialIcons} src={vk} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.twitter ? <a href={props.profile.contacts.twitter}> <img className={s.socialIcons} src={twitter} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.instagram ? <a href={props.profile.contacts.instagram}> <img className={s.socialIcons} src={insta} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.youtube ? <a href={props.profile.contacts.youtube}> <img className={s.socialIcons} src={youtube} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.github ? <a href={props.profile.contacts.github}> <img className={s.socialIcons} src={git} alt=""/></a> : ''}*/}
+                {/*{props.profile.contacts.mailLink ? <a href={props.profile.contacts.mailLink}> <img className={s.socialIcons} src={mail} alt=""/></a> : ''}*/}
             </div>
 
         </div>
