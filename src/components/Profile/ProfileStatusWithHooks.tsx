140380b894e1459cc9a23edc7b98d16885c274de
marina <marina.gherman@icloud.com>
2022-02-02 16:17:48 +0100

    add lesson 85( useEffect )

--- src/components/Profile/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type ProfileStatusType = {
     status: string
@@ -10,6 +10,10 @@ const ProfileStatusWithHooks = (props:ProfileStatusType) => {
 
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    },[props.status])
 
     const activateMode = () => {
         setEditMode(true)
